8c95b0d099068c806205ab4225047cea0186e8a2
rapidrecruitpro <rapidrecruitpro@gmail.com>
2025-08-07 01:26:48 +0100

    Update server.py

--- server.py
+++ server.py
@@ -64,4 +64,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+import os
+port = int(os.environ.get('port', 5000))
+app.run(host='0.0.0.0', port=port)
